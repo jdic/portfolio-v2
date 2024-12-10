e0f0334d29b5299273567fb71d028e8ca62aac69
jdi <jmotadv@gmail.com>
2024-12-09 18:49:18 -0700

    [FEAT] MDX Support and Page layout adaptation for MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
-
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig(
+{
   site: 'https://jmota.netlify.app/',
   integrations: [tailwind(), react(), mdx(), sitemap()],
-});
+  i18n: 
+  {
+    locales: ['en', 'es', 'fr'],
+    defaultLocale: 'en',
+    routing:
+    {
+      prefixDefaultLocale: false,
+    },
+    fallback:
+    {
+      fr: 'en',
+      es: 'en',
+    },
+  },
+})
