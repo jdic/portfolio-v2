fee2d4bf44f5b03b04e28e0b3b245a81c58a3056
jdi <jmotadv@gmail.com>
2024-12-10 20:21:51 -0700

    [FEAT] Section layout and tailwind theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,11 +21,30 @@ const addBasePlugin = ({ addBase }) =>
 export default
 {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ['class'],
 	theme:
 	{
 		extend:
 		{
-			
+			colors:
+			{
+				light:
+				{
+					text: '#ffffff',
+					background: '#000000',
+					primary: '#2f27ce',
+					secondary: '#ffd60a',
+					accent: '#4c6cfa',
+				},
+				dark:
+				{
+					text: '#000000',
+					background: '#ffffff',
+					primary: '#3a31d8',
+					secondary: '#f5cc00',
+					accent: '#0525b3',
+				}
+			},
 		},
 	},
 	plugins: [addBasePlugin],
