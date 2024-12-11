7d342b6252d01695aa0dbeb7adec2ab370cd2c40
jdi <jmotadv@gmail.com>
2024-12-10 23:09:07 -0700

    [FEAT] Badge component and astro-icon with iconify datasets

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig(
 {
   site: 'https://jmota.netlify.app/',
   prefetch: true,
-  integrations: [tailwind(), react(), mdx(), sitemap()],
+  integrations: [tailwind(), react(), mdx(), sitemap(), icon()],
   i18n: 
   {
     locales: ['en', 'es', 'fr'],
